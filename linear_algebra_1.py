3d2eeba320c7f32e65e90270457354277846c2ad
frankgrt <frankgrt@hotmail.com>
2017-06-13 22:24:39 +0800

    update

--- linear_algebra_1.py
+++ linear_algebra_1.py
@@ -1,5 +1,11 @@
 import math
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
 class Vector(object):
+
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -32,7 +38,7 @@ class Vector(object):
     def times_scalar(self, c):
         new_coordinates = [c*x for x in self.coordinates]
         return Vector(new_coordinates)
-    
+
     def magnitude(self):
         coordinates_squared = [x**2 for x in self.coordinates]
         return (sum(coordinates_squared))**0.5
@@ -42,9 +48,43 @@ class Vector(object):
             magnitude = self.magnitude()
             new_coordinates = [x/magnitude for x in self.coordinates]
             return new_coordinates
-        
+
         except ZeroDivisionError:
             raise Exception('Can not normalize the zero vector')
+
+    def dot_product(self,v):
+        return sum([x*y for x,y in zip(self.coordinates,v.coordinates)])
+
+    def angle_with(self,v,in_degree=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = acos(u1.dot(u2))
+
+            if in_degrees:
+                degrees_per_radian = 180. / pi
+                return angle_in_radians * degrees_per_radian
+            else:
+                return angle_in_radians
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot compute an angle with the zero vector')
+            else:
+                raise e # coding=utf-8
+
+
+
+
+
+
+vector_1 = Vector([3.183,-7.627])
+vector_2 = Vector([-2.668,5.319])
+print Vector.angle(vector_1,vector_2)
+
+vector_1 = Vector([7.35,0.221,5.188])
+vector_2 = Vector([2.751,8.259,3.985])
+print Vector.angle(vector_1,vector_2)/math.pi*180
+
 
 
 
@@ -66,10 +106,11 @@ print Vector.magnitude(verctor_6)
 verctor_7 = Vector([8.813,-1.331,-6.247])
 print Vector.magnitude(verctor_7)
 
-"""
+
 verctor_8 = Vector([5.581,-2.136])
 print verctor_8
 print verctor_8.normalize()
 
 verctor_9 = Vector([1.996,3.108,-4.554])
 print verctor_9.normalize()
+"""
